1517253a7c9e0a513e9a9e6836757e97b5928d2f
guningyi <ningyigu@gmail.com>
2017-05-09 15:46:46 +0800

    modify the qick sort

--- ALGORITHM/quickSort/V1/qSort.cpp
+++ ALGORITHM/quickSort/V1/qSort.cpp
@@ -4,45 +4,39 @@ using namespace std;
 
 int A[8] = {2,8,7,1,3,5,6,4};
 
-int partition(int array[], int p ,int r);
-void quickSort(int array[], int p, int r)
+int partition(int array[], int left ,int right);
+void quickSort(int array[], int left, int right)
 {
-    int q;
-    if ( p < r )
+    int position;
+    if ( left < right )
     {
-        q = partition(array, p, r);
-        cout<<"q="<<q<<endl;
-        quickSort(array,p,q-1); 
-        quickSort(array, q+1, r);   
+        position = partition(array, left, right);
+        quickSort(array,left,position-1); 
+        quickSort(array, position+1, right);   
     }
 }
 
-int partition(int array[], int p ,int r)
+int partition(int array[], int left ,int right)
 {
-    int x = array[r];
-    int i = p-1;
-    int j;
-    int temp;
-    for(j=p; j<r-1;j++)
-    {
-        if (array[j] > x)
-        {
-            j++;
-        }
-        else if(array[j] <= x )
-        {
-            i++;
-            temp = array[j];
-            array[j] = array[i];
-            array[i] = temp;
-            j++;
-        }
+    int base = array[left];
+    while(left < right){
+      while((left < right)&& array[right] >= base)
+      {
+	right--;   
+      }
+      if(array[right] < base)
+      {
+	array[left++] = array[right];
+      }
+      while((left < right) && array[left] < base){
+        left++;
+      }
+      if(array[left] >= base){
+        array[right--] = array[left];
+      }
     }
-    temp = x;
-    //cout<<"i="<<i<<endl;
-    array[r] = array[i+1];
-    array[i+1] = x;
-    return i+1;
+    array[left] = base;
+    return left;
 }
 
 int main(int argc, char **argv)
@@ -52,7 +46,7 @@ int main(int argc, char **argv)
     {
         cout<<"A["<<i<<"]="<<A[i]<<" "; 
     }
-    quickSort(A, 1, 7);
+    quickSort(A, 0, 7);
     cout<<"\n"<<"after quick sort!"<<endl;
     for (i=0;i<8;i++)
     {
